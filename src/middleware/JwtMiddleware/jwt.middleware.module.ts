d4fe367bffd477f002a8a6c358fab7ccb34c62f0
DiffickMenLogo <gh.sdfs@mail.ru>
2023-04-09 16:29:43 +0300

    feat: add cors to localhost

--- src/middleware/JwtMiddleware/jwt.middleware.module.ts
+++ src/middleware/JwtMiddleware/jwt.middleware.module.ts
@@ -14,6 +14,18 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 })
 export class JwtMiddlewareModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
+    consumer.apply((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // указываем домен, с которого разрешены запросы
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+      ); // указываем список разрешенных заголовков
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS',
+      ); // указываем список разрешенных методов запросов
+      next();
+    });
     consumer
       .apply(JwtMiddleware)
       .exclude('auth/signup', 'auth/signin', 'words', 'files/:filename')
