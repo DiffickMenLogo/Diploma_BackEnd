85657218e587976ea7753c47f43c5bcfcde5e9ff
DiffickMenLogo <gh.sdfs@mail.ru>
2023-05-02 21:51:13 +0300

    fix: make exports to controllers

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -2,10 +2,12 @@ import { SettingsService } from './settings.service';
 import { SettingsEntity } from './entities/settings.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { SettingsController } from './settings.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SettingsEntity])],
   providers: [SettingsService],
+  controllers: [SettingsController],
   exports: [SettingsService],
 })
 export class SettingsModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,12 @@ import { UserEntity } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UserService],
+  controllers: [UserController],
   exports: [UserService],
 })
 export class UserModule {}
